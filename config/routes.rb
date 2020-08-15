1773c460197be59c15bbcaf2bb70bc61762b4cf4
NAKAGAKI Kenji <nakaken0629@gmail.com>
2020-08-15 10:51:41 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'application#hello'
+  get 'static_pages/contact'
 end
