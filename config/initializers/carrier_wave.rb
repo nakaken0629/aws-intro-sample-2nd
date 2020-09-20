5c1b6fb99867cf2e8f6cf1d96abd099c2c1bf364
NAKAGAKI Kenji <nakaken0629@gmail.com>
2020-09-20 17:57:54 +0900

    add s3 config to production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider        => 'AWS',
-      :region          => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :region          => ENV['AWS_INTRO_SAMPLE_S3_REGION'],     # 例: 'ap-northeast-1'
       :use_iam_profile => true
     }
-    config.fog_directory =  ENV['S3_BUCKET']
+    config.fog_directory =  ENV['AWS_INTRO_SAMPLE_S3_BUCKET']
   end
 end
